76ee787611da6bd83bf3111ec6dd3231fd45783d
jerrytek <jerrycodingtek@gmail.com>
2021-12-16 15:44:34 -0600

    good code only

---
+++ controllers/posts.js
@@ -0,0 +1,11 @@
+import Post from '../models/post.js'
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+    res.json(posts)
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({error: error.message})
+  }
+}
