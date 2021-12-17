6f4e4327b1bcd67213e9711b9e04671bd5a07e8a
Michael Tan <michaeltan@MacBook-Pro.fios-router.home>
2021-12-17 10:12:42 -0500

    added the post by id

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,3 +9,18 @@ export const getPosts = async (req, res) => {
     res.status(500).json({error: error.message})
   }
 }
+
+export const getPost = async (req, res) => {
+  try {
+    const { id } = req.params
+    const post = await Post.findById(id)
+    if (post) {
+      return res.json(product)
+    }
+    res.status(404).json({ message: 'Product not found!'})
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ error: error.message})
+  }
+}
+
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,7 @@ import * as controllers from '../controllers/posts.js'
 const router = Router()
 
 router.get('/posts', controllers.getPosts)
+router.get('/posts/:id', controllers.getPosts)
+
 
 export default router
