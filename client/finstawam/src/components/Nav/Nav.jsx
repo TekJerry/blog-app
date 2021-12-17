c26ef5ad4f659d35f37ddc0ced18fd4bdc98cf85
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2021-12-17 15:07:44 -0500

    Navlink

--- client/finstawam/src/components/Nav/Nav.jsx
+++ client/finstawam/src/components/Nav/Nav.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import '..Nav/Nav.css'
+import { NavLink } from 'react-router-dom'
+
 
 export default function Nav() {
     return (
-        <div>
+        <nav>
+        <div className= "nav">
+            <NavLink className = "logo" to='/'>Finstawam</NavLink>
+            <div className="links">
+                <NavLink className="link" to="/newpost">New Post</NavLink>
+                
+            </div>
             
         </div>
+        </nav>
     )
 }
